299cc384087e610206bc698e7940b3d891c10dcc
Hylondon <deboligali@gmail.com>
2026-08-27 01:40:01 +0100

    update user

--- quicktxn-client/src/lib/dashboard.ts
+++ quicktxn-client/src/lib/dashboard.ts
@@ -1,13 +1,16 @@
 import api from "@/lib/api";
 
 export const getDashboardData = async () => {
-    const [wallet, transactions, notifications] = await Promise.all([
-        api.get("/wallet/balance"),
-        api.get("/transactions"),
-        api.get("/notifications"),
-    ]);
+    const [user, wallet, transactions, notifications] =
+        await Promise.all([
+            api.get("/user/profile"),
+            api.get("/wallet/balance"),
+            api.get("/transactions"),
+            api.get("/notifications"),
+        ]);
 
     return {
+        user: user.data.data,
         wallet: wallet.data.data,
         transactions: transactions.data.transactions,
         notifications: notifications.data.data,
