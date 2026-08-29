7eba30bf11cc87035740861f8ae67ed400538352
Hylondon <deboligali@gmail.com>
2026-08-29 21:56:29 +0100

    update

--- server/app.js
+++ server/app.js
@@ -117,7 +117,7 @@ app.use("/api/notifications", notificationRoutes);
 // Contact & Public
 // ==========================
 app.use("/api/contact", contactRoutes);
-app.use("/api/public", publicRoutes);c
+app.use("/api/public", publicRoutes);
 
 // ==========================
 // Paystack
